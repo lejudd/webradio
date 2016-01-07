e373b1ebf7d090c1f5a367732271b251988878be
Leighton <leighton.judd@gmail.com>
2016-01-07 20:55:20 +1100

    Update control.js

--- control.js
+++ control.js
@@ -8,57 +8,59 @@ var reconnectTimeout = 2000;
 var reconnectTries = 2;
 var retry = reconnectTries;
 
-//audiocgi = "/cgi-bin/audio.sh";
-
-var ws;
+var socket;
 
 function connect() {
-	var ws = new WebSocket("ws://" + location.hostname + "/ws/");
+	socket = io();
 	
-	ws.onopen = function() {
-	        $("#selectFreq").css("background-color", "#cfc");
+	socket.on("connect", function() {
+		$("#selectFreq").css("background-color", "#cfc");
+		$("#display").css("background-color", "#cfc");
 		reTry = reconnectTries;
-	};
+	});
 
-	ws.onclose = function() {
-	        $("#selectFreq").css("background-color", "#aaa");
+	socket.on("disconnect", function() {
+		$("#selectFreq").css("background-color", "#aaa");
+		$("#display").css("background-color", "#aaa");
 		if (retry > 0) {
 			setTimeout(connect, reconnectTimeout);
 			retry--;
 		};
 		if (retry < 1) {
 		};
-	};
+	});
 
-	ws.onerror = function() {
-	        $("#selectFreq").css("background-color", "#a00");	
-	};
+	socket.on("error", function() {
+		$("#selectFreq").css("background-color", "#a00");
+		$("#display").css("background-color", "#a00");
+	});
 
-	ws.onmessage = function(event) {
-		var ev = event.data;
-		var data = event.data.split(" ");
+	socket.on("command", function(msg) {
+		var data = msg.split(" ");
 		if (data.length > 1) {
 			var val = data[0];
 			if (val == "FREQ") {
 				setFreq(data[1]);
 			}
 			else if (val == "SIG") {
+				var sig = (parseFloat(data[1]) + 60.0).toString();
 				$("#signalLevel").slider("disable");
-				$("#signalLevel").val(data[1]).slider("refresh");
+				$("#signalLevel").val(sig).slider("refresh");
 			}
 			else if (val == "MODE") {
 				setMode(data[1]);
 				setBandwidth(data[2]);
 			}
 			else if (val == "SQL") {
-				if (data[1] > 0) {
-					currentSquelch = data[1];
+				var sql = (parseFloat(data[1]) * 100.0).toString();
+				if (sql > 0) {
+					currentSquelch = sql;
 				}
-				$("#squelchValue").val(data[1]);
-				setSquelch(data[1]);
+				$("#squelchValue").val(sql);
+				setSquelch(sql);
 			}
 		}
-	};
+	});
 
 	audio = new Audio("");
 	
@@ -74,8 +76,10 @@ function connect() {
 		audio.src = "";
 		$("#audioStart").html("Start Audio");
 	});
+
+	$("#display").mousedown(changeDigit);
 	
-	$("#selectFreq").change(changeFreq);
+	//$("#selectFreq").change(changeFreq);
 
 	$("#selectMode").change(changeModeBW);
 	
@@ -85,6 +89,7 @@ function connect() {
 	
 	$("#selectFreq").keypress(function(e) {
 		if (e.which == 13) {
+			$("#Enter").click();
 			$("#selectFreq").blur();
 		}
 	});
@@ -103,7 +108,7 @@ function connect() {
 		if (newSql > 1) {
 			newSql = newSql / 100;
 		}
-		ws.send("L SQL " + newSql);
+		socket.emit("command", "L SQL " + newSql);
 
 		setSquelch(newSql);
 	});
@@ -169,7 +174,7 @@ function connect() {
 			bandwidth = "230000";
 		else if (mode == "CW" || mode == "CWR" || mode == "LSB" || mode == "USB")
 			bandwidth == "2800";
-		ws.send("M " + mode + " " + bandwidth);
+		socket.emit("command", "M " + mode + " " + bandwidth);
 		$("#selectBandwidth").blur();
 		$("#selectMode").blur();
 	};
@@ -196,15 +201,19 @@ function connect() {
 
 	function changeFreq() {
 		var newFreq = $("#selectFreq").val();
-		if (newFreq != currentFreq) {
-			if (newFreq < 2000) {
-				newFreq = newFreq * 1000000;
-			}
-			ws.send("F " + newFreq);
+		newFreq = newFreq * 1000000;
+		if (newFreq != currentFreq && newFreq > 0) {
+			socket.emit("command", "F " + newFreq);
 		}
 	};
 
 	function setFreq(freq) {
+		var disp = freq;
+		currentFreq = freq;
+		var pad = "0000000000";
+		disp = pad.substring(0, pad.length - disp.length) + disp;
+		disp = disp.substring(0, 4) + "." + disp.substring(4, 7) + "." + disp.substring(7, 10);
+		display.setValue(disp);
 		if (freq > 2000) {
 			freq = freq / 1000000;
 		}
@@ -212,13 +221,13 @@ function connect() {
 	};
 	
 	function setMode(mode) {
-		$("#selectMode").val([]);
-		$("#selectBandwidth").val([]);
+		//$("#selectMode").val("");
 		$("#selectMode option[value=" + mode + "]").attr("selected", "selected");
 		$("#selectMode").selectmenu("refresh");
 	};
 	
 	function setBandwidth(mode) {
+		//$("#selectBandwidth").val("");
 		if (mode == "2800")
 			$("#selectBandwidth option[value=Medium]").attr("selected", "selected");
 		if (mode == "6000")
@@ -258,12 +267,79 @@ function connect() {
 	
 		srate = $("#selectARate").val();
 		localStorage.audioRate = srate;
-		$("#selectARate").val([]);
+		$("#selectARate").val("");
 		$("#selectARate option[value=" + srate + "]").attr("selected", "selected");
 		$("#selectARate").selectmenu("refresh");
 	};
+	
+	function changeDigit(event) {
+		// -- Map of digits --
+		// Digit 1 X: 0 25 	Y: 86
+		// Digit 2 X: 30 55	Y: 86
+		// Digit 3 X: 60 80	Y: 86
+		// Digit 4 X: 85 110	Y: 86
+		// Digit 5 X: 125 145	Y: 86
+		// Digit 6 X: 150 175	Y: 86
+		// Digit 7 X: 180 205	Y: 86
+		// Digit 8 X: 220 245	Y: 86
+		// Digit 9 X: 250 270	Y: 86
+		// Digit 10 X: 275 300	Y: 86
 
+		var cx = event.pageX;
+		var cy = event.pageY;
+		
+
+		var centre = 86;
+
+		var points = {1:25, 2:55, 3:80, 4:110, 5:145, 6:175, 7:205, 8:245, 9:270, 10:300};
+
+		for (var kv in points) {
+			if (cx < points[kv]) {
+				var pad = "0000000000";
+				var freq = pad.substring(0, pad.length - currentFreq.length) + currentFreq;
+
+				var digit = freq.substr(kv - 1, 1);
+				
+				var ud;
+
+				if (cy < centre) {
+					digit++;
+					if (digit > 9)
+						digit = 0;
+				}
+				if (cy > centre) {
+					digit--;
+					if (digit < 0)
+						digit = 9;
+				}
+
+				freq = freq.substr(0, kv - 1) + digit + freq.substr(kv);
+				freq = parseInt(freq);
+
+				if (freq != currentFreq) {
+					socket.emit("command", "F " + freq);
+				}
+
+				break;
+			}
+		}
+	}
 };
+
+// 7-segment display code
+var display = new SegmentDisplay("display");
+display.pattern         = "####.###.###";
+display.displayAngle    = 6;
+display.digitHeight     = 20;
+display.digitWidth      = 14;
+display.digitDistance   = 4.0;
+display.segmentWidth    = 2.5;
+display.segmentDistance = 0.3;
+display.segmentCount    = 7;
+display.cornerType      = 3;
+display.colorOn         = "#090909";
+display.colorOff        = "#e2e2e2";
+display.draw();
 
 $(document).ready(function () {
 	connect();
